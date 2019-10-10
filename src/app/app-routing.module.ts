5e5278fe8340423ce43e51e364d771df3418d842
PRIMADANA\prima <bellaarifprimadana@gmail.com>
2019-10-10 18:22:10 +0700

    debug routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'registration', pathMatch: 'full' },
   {
-    path: '',
-    loadChildren: () => import('./registration-form/registration-form.module').then(m => m.RegistrationFormModule),
-  }
+    path: 'registration',
+    component: RegistrationFormComponent,
+  },
 ];
 
 @NgModule({
