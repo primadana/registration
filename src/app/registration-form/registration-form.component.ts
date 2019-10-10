4c198b19694e2371509102c8c04d4c946e2c7787
PRIMADANA\prima <bellaarifprimadana@gmail.com>
2019-10-10 21:15:59 +0700

    layout done

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-registration-form',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
+  month: Month[] = [
+    {value: '1', viewValue: 'January'},
+    {value: '2', viewValue: 'February'},
+    {value: '3', viewValue: 'March'},
+    {value: '4', viewValue: 'April'},
+    {value: '5', viewValue: 'May'},
+    {value: '6', viewValue: 'June'},
+    {value: '7', viewValue: 'July'},
+    {value: '8', viewValue: 'August'},
+    {value: '9', viewValue: 'September'},
+    {value: '10', viewValue: 'October'},
+    {value: '11', viewValue: 'November'},
+    {value: '12', viewValue: 'December'},
+  ];
+
+  date: Date[] = [];
+  year: Year[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.countDate();
+    this.countYear();
   }
 
+  countDate(){
+    let str = [];
+    for(let i=1; i <= 31; i++){
+      str.push({value: i, viewValue: i});
+    }
+    this.date = str;
+  }
+
+  countYear(){
+    let str = [];
+    let date = new Date();
+    let year = { year: date.getFullYear()}
+    let year_end = year.year - 10
+    let year_start = year_end - 40;
+    for(let i=year_end; i >= year_start; i--){
+      str.push({value: i, viewValue: i});
+    }
+    this.year = str;
+  }
+
+}
+
+export interface Month {
+  value: string;
+  viewValue: string;
+}
+export interface Date {
+  value: string;
+  viewValue: string;
+}
+export interface Year {
+  value: string;
+  viewValue: string;
 }
